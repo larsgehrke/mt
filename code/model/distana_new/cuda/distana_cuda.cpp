db8652a2cf6207e3ae4cb5a7699e4660aafa082b
Lars Gehrke <accounts@larsgehrke.de>
2021-02-05 17:17:42 +0100

    starting to implement the distana cuda kernel

--- code/model/distana_new/cuda/distana_cuda.cpp
+++ code/model/distana_new/cuda/distana_cuda.cpp
@@ -24,7 +24,7 @@ std::vector<torch::Tensor> distana_cuda_backward(
 
 // C++ interface
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
