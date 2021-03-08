b42fe68316fbaa8e9934e88b5d491fd4ef940823
Lars Gehrke <accounts@larsgehrke.de>
2021-03-08 18:04:26 +0100

    cuda graph connections creation

--- code/dev/model/th2/kernel_net.py
+++ code/dev/model/th2/kernel_net.py
@@ -58,7 +58,7 @@ class KernelNetwork(th.nn.Module):
         x = self.graph.forward(self.tensors.pk_lat_out)
         #self.tensors.pk_lat_in[:,self.pos0, self.going_to] = \
         #self.tensors.pk_lat_out[:,self.coming_from, self.going_to]
-        sprint(x, "x" , exit= True)
+        sprint(x, "x", exit= True)
 
         if iter_idx == 5 and t == 5:
             clock.split("Graph connections")
