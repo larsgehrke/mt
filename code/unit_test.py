ea12d237dba3af6e930d8a133dfd24b5ba13f46e
Lars Gehrke <accounts@larsgehrke.de>
2021-03-18 21:13:49 +0100

    unit test graph 2 from th3

--- code/unit_test.py
+++ code/unit_test.py
@@ -12,11 +12,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description, 
             formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('unit_test', choices=['graph'],  
+    parser.add_argument('unit_test', choices=['graph', 'graph2'],  
             help='Which unit test should be executed?')
 
     options = parser.parse_args()
 
     if options.unit_test == "graph":
         from qa.test_graph_cuda import test_graph
+        test_graph()
+    if options.unit_test == "graph2":
+        from qa.test_graph_cuda2 import test_graph
         test_graph()
