32171640c6ea5177593f8e53743bc27c10842113
Lars A. Gehrke <accounts@larsgehrke.de>
2021-04-27 15:02:45 +0200

    eval speed test

--- code/evaluation.py
+++ code/evaluation.py
@@ -12,7 +12,7 @@ for b in batches:
         data = np.zeros(10)
         for i in range(10):
             x = os.popen(f"python test.py -m {m} -b {b}").read()
-            data[i] = (float)x
+            data[i] = float(x)
         mean = np.mean(data)
         stddev = np.std(data)
         print(f"[batch size {b}, model {m}] mean: {mean}, stddev: {stddev}")
