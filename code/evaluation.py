23947eb7bdcadc6cb35665059d519597a18bbbf2
Lars A. Gehrke <accounts@larsgehrke.de>
2021-04-27 15:13:25 +0200

    eval speed test

--- code/evaluation.py
+++ code/evaluation.py
@@ -9,10 +9,11 @@ models = ["v1a", "v1b"]
 
 for b in batches:
     for m in models:
-        data = np.zeros(100)
-        for i in range(100):
+        data = np.zeros(10)
+        for i in range(10):
             x = os.popen(f"python test.py -m {m} -b {b}").read()
             data[i] = float(x)
+            print(data[i])
         mean = np.mean(data)
         mean = np.around(mean, decimals=5)
         stddev = np.std(data)
