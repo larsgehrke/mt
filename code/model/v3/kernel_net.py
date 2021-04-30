721740a4f56b1f027b3e9052ae349596527fbd06
Lars A. Gehrke <accounts@larsgehrke.de>
2021-04-30 12:28:13 +0200

    checking backward pass of graph2 (v3)

--- code/model/v3/kernel_net.py
+++ code/model/v3/kernel_net.py
@@ -77,7 +77,7 @@ class KernelNetwork(th.nn.Module):
             
             input_ = th.cat((self.tensors.pk_dyn_in, lat_in_flat),2)
 
-        sprint(input_.shape, "input_.shape", exit=True)
+        sprint(input_, "input_.shape", exit=True)
 
         return input_
 
