49d4c84223c79c6b41d8f95dc1a1fd4aa6453110
Lars A. Gehrke <accounts@larsgehrke.de>
2021-05-22 13:02:26 +0200

    eval train automatic version

--- code/evaluation.py
+++ code/evaluation.py
@@ -7,30 +7,21 @@ import numpy as np
 batches = ["1", "2", "4", "8", "16", "32", "64", "128"]
 models =  ["v1a", "v1b", "v2", "v3"]
 
-out = ""
+rows = []
+row = ""
 
-for b in batches:
+for idx,b in enumerate(batches):
+    row[idx] = str(b)
+    print("batch size " + str(b)+ ":")
     for m in models:
+        res = "model " + str(m) + ": "
         command = f"python train.py -b {b} -m {m}"
         x = os.popen(command).read()
-        out = out + " & " + str(x)  
+        print(res + str(x))
+        row[idx] = row[idx] + " & " + str(x)  
         
-    print(b + out +"\\\\")
+    row[idx] = row[idx] + "\\\\"
 
-
-print()
-print()
-print()
-out = "Order batches: "
-for b in batches:
-    out = out + str(b)+", "
-print(out)
-out = "Order models: "
-for m in modelss:
-    out = out + str(m)+", "
-
-
-
-
-
+for r in rows:
+    print(str(r))
 
