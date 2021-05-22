c6d1945ad0a766a55b8d9365e3d6134c8356ed0f
Lars A. Gehrke <accounts@larsgehrke.de>
2021-05-22 13:04:21 +0200

    eval train automatic version

--- code/evaluation.py
+++ code/evaluation.py
@@ -8,19 +8,20 @@ batches = ["1", "2", "4", "8", "16", "32", "64", "128"]
 models =  ["v1a", "v1b", "v2", "v3"]
 
 rows = []
-row = ""
+row
 
 for idx,b in enumerate(batches):
-    rows[idx] = str(b)
+    row = str(b)
     print("batch size " + str(b)+ ":")
     for m in models:
         res = "model " + str(m) + ": "
         command = f"python train.py -b {b} -m {m}"
         x = os.popen(command).read()
         print(res + str(x))
-        rows[idx] = rows[idx] + " & " + str(x)  
+        row = row + " & " + str(x)  
         
-    rows[idx] = rows[idx] + "\\\\"
+    row = row + "\\\\"
+    rows.append(row)
 
 for r in rows:
     print(str(r))
