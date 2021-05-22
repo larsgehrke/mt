a20419e3bfac423d6bc376707952a867540445fa
Lars A. Gehrke <accounts@larsgehrke.de>
2021-05-22 15:08:10 +0200

    check forward/backward pass automatically

--- code/evaluation.py
+++ code/evaluation.py
@@ -7,24 +7,25 @@ import numpy as np
 batches = ["1", "2", "4", "8", "16", "32", "64", "128"]
 old_models = ["old", "old2"]
 new_models =  ["v1a", "v1b", "v2", "v3"]
-device = ["False", "True"]
+use_gpu_values = ["False", "True"]
 
 rows = []
 row = ""
-for d in device:
-    print(f"=== use gpu? {d} ===")
+for use_gpu in enumerate(use_gpu_values):
+    print(f"=== use gpu? {use_gpu} ===")
     for om in old_models:
         print(f"= version {om} =")
-        command = f"python train.py --use-gpu {d} -b 1 -m {om}"
+        command = f"python train.py --use-gpu {use_gpu} -b 1 -m {om}"
         x = os.popen(command).read()
         print(x)
     print()
     for b in batches:
         print("== batch size " + str(b)+ " ==")
-        for nm in new_models:
-            print(f"= version {nm} =")
-            command = f"python train.py --use-gpu {d} -b {b} -m {nm}"
-            x = os.popen(command).read()
-            print(x)
+        for nm in enumerate(new_models):
+            if not(use_gpu=="False" and (nm=="v2" or nm=="v3")):   
+                print(f"= version {nm} =")
+                command = f"python train.py --use-gpu {use_gpu} -b {b} -m {nm}"
+                x = os.popen(command).read()
+                print(x)
         print()
 
